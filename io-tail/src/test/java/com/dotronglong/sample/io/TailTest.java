7ec6c5971a3c450b0515258241a73f6c5a4ac59a
Long Do <me@dotronglong.com>
2018-08-26 12:51:54 +0700

    Implement tail

--- io-tail/src/test/java/com/dotronglong/sample/io/TailTest.java
+++ io-tail/src/test/java/com/dotronglong/sample/io/TailTest.java
@@ -11,13 +11,14 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TailTest {
     private Tail tail = new Tail();
     private static char[] chars;
     private static String filePath;
-    private static boolean createOnce = true;
+    private static boolean initOnce = true;
+    private static int numberOfLines = 50;
 
     static {
         int min = 32;
@@ -33,14 +34,14 @@ public class TailTest {
 
     @BeforeClass
     public static void setUp() throws IOException {
-        if (createOnce && (new File(filePath).exists())) {
+        if (initOnce && (new File(filePath).exists())) {
             // Only create file one time
             System.out.println("File is created already");
             return;
         }
 
         // Create a file which has random data (and lines)
-        long maxFileSize = 2 * 1024 * 1024; // in Mb
+        long maxFileSize = 80 * 1024 * 1024; // in Mb
         BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, false));
         Stopwatch stopwatch = Stopwatch.createStarted();
 
@@ -70,10 +71,28 @@ public class TailTest {
     }
 
     @Test
-    public void testReadLines() {
-        int numberOfLines = 20;
+    public void testReadLines() throws IOException {
+        testTailLines(numberOfLines);
+        testTailUnix(numberOfLines);
+    }
+
+    private void testTailLines(int numberOfLines) throws IOException {
+        Stopwatch stopwatch = Stopwatch.createStarted();
         List<String> lines = tail.readLines(filePath, numberOfLines);
-        assertTrue(lines.size() <= numberOfLines);
-        System.out.println("Total Lines: " + lines.size());
+        System.out.println("tail.readLines finishes in " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+        assertEquals(numberOfLines, lines.size());
+    }
+
+    private void testTailUnix(int numberOfLines) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command("tail", "-n" + numberOfLines, filePath);
+        try {
+            Process process = builder.start();
+            process.waitFor();
+            System.out.println("tail finishes in " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
